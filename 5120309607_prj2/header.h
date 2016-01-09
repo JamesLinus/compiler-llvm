247a41f4c355a78ac25a32c92bafa81b7c3c34d7
jiady <dongyuj.vip@gmail.com>
2016-01-09 00:29:31 -0800

    basic prj2

---
+++ 5120309607_prj2/header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h> 
+#include <stdarg.h> 
+#include <cstdio>
+
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+
+#endif
